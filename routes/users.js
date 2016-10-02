8e4fe3609d31fc0676aec07aac5310886c9c183f
aviaryan <avi.aryan123@gmail.com>
2016-10-02 19:46:16 +0530

    created user POST API

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var crypto = require('crypto')
 
 var User = require('../models/User.js')
 
@@ -14,5 +15,28 @@ router.get('/', function (req, res, next) {
     res.json(users)
   })
 })
+
+/*
+ * Create new user
+ */
+router.post('/', function (req, res, next) {
+  var user = new User(req.body)
+  // set default username
+  if (!req.body.username) {
+    user.username = user.email
+  }
+  // set hash password
+  var hashPassword = crypto.createHash('sha1')
+  hashPassword.update(user.password)
+  user.password = hashPassword.digest('hex')
+  // save user
+  user.save(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.json(user)
+  })
+})
+
 
 module.exports = router
