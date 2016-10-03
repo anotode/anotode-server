bc29050bcbf92030096c01be5c03a468f9f2da97
boddu-manohar <201451024@iiitvadodara.ac.in>
2016-10-04 01:00:24 +0530

    changed color to String.And made user_id non-unique

--- models/Highlight.js
+++ models/Highlight.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose')
 
 var HighlightSchema = new mongoose.Schema({
-  user_id: { type: String, unique: true },
+  user_id: String,
   title: String,
   text: String,
   url: String,
   category: String,
-  color: Number,
+  color: String,
   timeDate: Number,
   comment: String,
   tags: [ String ]
